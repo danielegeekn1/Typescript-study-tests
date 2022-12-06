20b23517711a19d5a23bb1d415a7f60b766f9a8d
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-06 19:22:16 +0100

    example of variable with a class set to it as type

--- classes-in-typescript.ts
+++ classes-in-typescript.ts
@@ -17,3 +17,14 @@ const inVoice2 = new inVoice("Luigi", "Loan", 850);
 console.log(inVoice1, inVoice2);
 console.log(inVoice1.format());
 console.log(inVoice2.format());
+
+let invoices: inVoice[] = []; //only classes prop may be passed into this empty array
+invoices.push(inVoice1);
+invoices.push(inVoice2);
+//we can only push to this array only element of the created class we passed to it as type
+console.log(invoices);
+inVoice1.client = "Luca";
+inVoice2.amount = 400;
+
+//since we instanciated our class, we created new variable, that'll be public and so we'll be able to
+//modify each of their values with dot notations
