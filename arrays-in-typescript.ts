59771fc44c7edf5899d64a4963979e55379bed3d
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-06 15:25:26 +0100

    read only array of string addded

--- arrays-in-typescript.ts
+++ arrays-in-typescript.ts
@@ -10,3 +10,28 @@ countries.push("Japan", 43);
 
 //since we define explicitly the types of our constant to be an array of only string and number types
 //we can no longer add other tpyes for example boolean or we'll have an error
+interface bandsProperties {
+  name: string;
+  members: number;
+}
+const bands: bandsProperties[] = [
+  {
+    name: "Beatles",
+    members: 4,
+  },
+  {
+    name: "Pink Floyd",
+    members: 6,
+  },
+  {
+    name: "Queen",
+    members: 6,
+  },
+];
+
+const brands: readonly string[] = ["Apple, Samsung", "Asus"];
+
+//by default we could modify our array, since we pass only values of the type we set
+// in our case string types
+
+//if we do not want this to be modified we can use on our const brand a read only property modifier
