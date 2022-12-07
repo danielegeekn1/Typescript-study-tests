784d701ab8da2b03cfaf209bd87119a91bcbe481
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-07 11:08:07 +0100

    intersection types in object

--- intersectionTypes.ts
+++ intersectionTypes.ts
@@ -11,3 +11,20 @@ const describe = (country: Country & Climate) => {
 describe({ name: "Nigeria", desert: true });
 describe({ name: "New Zealand", desert: false });
 describe({ name: "Narnia", desert: false });
+
+//intersection types applied to an object
+interface Nations {
+  name: string;
+  code: string;
+}
+interface NationsData {
+  languages: string[];
+  population: number;
+}
+type CountryWithData = Nations & NationsData;
+const Greece: CountryWithData = {
+  name: "Greece",
+  code: "GR",
+  languages: ["Greek"],
+  population: 456_666_987,
+};
