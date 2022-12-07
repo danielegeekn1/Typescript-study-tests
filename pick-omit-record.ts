83ce12074e65c4fdbc225d850e3c921f23fd96aa
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-07 16:51:43 +0100

    pixk and omit record example

--- pick-omit-record.ts
+++ pick-omit-record.ts
@@ -19,3 +19,33 @@ const country1: Country = {
 };
 //with pick method we choose which properties we want to use( only the properties we want to use)
 //with omit method we choose which properties we want to omit ( only the one we do not want to use)
+
+//RECORD
+const anime: Record<string, string | number> = {
+  title: "One Piece",
+  author: "Eichiro Oda",
+  age: 47,
+};
+interface AnimeInfo {
+  title: string;
+  author: string;
+  age: number;
+}
+type animeDetails = Record<"first" | "second" | "third", AnimeInfo>;
+const anime1: animeDetails = {
+  first: {
+    title: "One Piece",
+    author: "Eichiro Oda",
+    age: 47,
+  },
+  second: {
+    title: "Naruto",
+    author: "Masashi Kishimoto",
+    age: 49,
+  },
+  third: {
+    title: "Vagabond",
+    author: "Takehiko Inoure",
+    age: 45,
+  },
+};
